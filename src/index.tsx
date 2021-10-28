e632231e33b7d8b4ddbf39c9bc3900783a949716
Настя <ablovatskaja@gmail.com>
2021-10-28 10:08:39 +0300

    add  useReducer

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AppWithUseReducer from './AppWithUseReducer';
 
 ReactDOM.render(
-    <App />,
+    <AppWithUseReducer />,
   document.getElementById('root')
 );
 
